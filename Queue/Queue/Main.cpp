07b8cf4d3a3c6506f0a76e3c47f24e140725b26c
QuanVAMK <nguyenquan261098@gmail.com>
2018-04-30 09:23:17 +0300

    Improvement of using Stacks to implement Queue

--- Queue/Queue/Main.cpp
+++ Queue/Queue/Main.cpp
@@ -1,14 +1,74 @@
+// Implementation of Queue using 2 Stacks
 #include <iostream>
-#include "Queue.h"
+
+struct Node 
+{
+	int Data;
+	Node *Next;
+};
+
+struct Queue 
+{
+	Node *Stack1;
+	Node *Stack2;
+};
+
+void PushStack(Node *&OutTop, int Item)
+{
+	Node *Temp = new Node();
+	Temp->Data = Item;
+	Temp->Next = OutTop;
+	OutTop = Temp;
+}
+
+int PopStack(Node *&OutTop)
+{
+	Node *Temp = OutTop;
+	OutTop = Temp->Next;
+	int x = Temp->Data;
+	delete Temp;
+
+	return x;
+}
+
+// TODO: Method when EnQueue is costly
+void EnQueue(Queue *&OutQ, int Item)
+{
+	PushStack(OutQ->Stack1, Item);
+}
+
+int Dequeue(Queue *&OutQ)
+{
+	if (!OutQ->Stack1 && !OutQ->Stack2) {
+		return -1;
+	}
+
+	// If Stack2 is empty then push Items from Stack1 to Stack2
+	if (!OutQ->Stack2) {
+		while (OutQ->Stack1) {
+			int x = PopStack(OutQ->Stack1);
+			PushStack(OutQ->Stack2, x);
+		}
+	}
+
+	// Pop Item from Stack2
+	return PopStack(OutQ->Stack2);
+}
 
 int main()
 {
-	Queue q1 = Queue(10);
+	Queue *q1 = new Queue();
+	q1->Stack1 = nullptr;
+	q1->Stack2 = nullptr;
 
-	q1.Push(10);
-	q1.Push(20);
-	q1.Push(30);
+	EnQueue(q1, 3);
+	EnQueue(q1, 2);
+	EnQueue(q1, 1);
+	EnQueue(q1, 4);
+	EnQueue(q1, 5);
 
-	std::cout << q1.Pop();
+	Dequeue(q1);
+	Dequeue(q1);
+	Dequeue(q1);
 	return 0;
 }
