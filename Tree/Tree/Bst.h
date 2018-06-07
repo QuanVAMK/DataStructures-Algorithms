c9042a92416f2c357aed36e7b0cf8c7de2533475
QuanVAMK <nguyenquan261098@gmail.com>
2018-06-07 21:00:05 +0700

    Fix & implement LevelOrderTraversal using Queue

--- Tree/Tree/Bst.h
+++ Tree/Tree/Bst.h
@@ -6,10 +6,44 @@ struct BstNode {
 	BstNode *Right;
 };
 
+// Custom Queue to hold pointers to BstNode
+class Queue
+{
+private:
+	// The max amount of elements in Queue
+	int Capacity;
+
+	// Determines current size of Queue, checks if Queue is empty or not
+	int Size;
+
+	// Points to the first in line queue
+	int Front;
+
+	// Points to the last in line queue
+	int Rear;
+
+	// Array holding pointers to BstNode
+	BstNode **Arr;
+
+public:
+	Queue(int NewCapacity);
+	~Queue();
+
+	bool IsEmpty();
+
+	void EnQueue(BstNode *NewNode);
+	BstNode *DeQueue();
+
+};
+
 void Insert(BstNode *&Root, int Data);
 
-// Return True if Sera
+// Return True if the element exists in the tree
 bool IsSearchable(const BstNode *&Root, int Data);
+
+int FindTreeHeight(const BstNode *&Root);
+
+void LevelOrderTraversal(BstNode *&Root);
 
 // Initialize & Return a new Node
 BstNode *SetNewNode(int Data);
--- Tree/Tree/Main.cpp
+++ Tree/Tree/Main.cpp
@@ -3,6 +3,17 @@
 #include "Bst.h"
 
 
+// Helper function to find max number between 2 numbers
+int FindMaxOfTwoNums(int N1, int N2)
+{
+	if (N1 >= N2) {
+		return N1;
+	} else {
+		return N2;
+	}
+}
+
+// Helper function to set a new Node
 BstNode *SetNewNode(int Data)
 {
 	BstNode *NewNode = new BstNode();
@@ -26,7 +37,7 @@ void Insert(BstNode *&Root, int Data)
 
 bool IsSearchable(BstNode *&Root, int Data)
 {
-	// If Root is empty we didn't find 
+	// No element found if Tree is empty
 	if (!Root) {
 		return false;
 	} else {
@@ -40,6 +51,84 @@ bool IsSearchable(BstNode *&Root, int Data)
 	}
 }
 
+int FindTreeHeight(BstNode *&Root)
+{
+	// Empty subtree
+	if (!Root) {
+		return -1;
+	}
+
+	int LeftTreeHeight = FindTreeHeight(Root->Left);
+	int RightTreeHeight = FindTreeHeight(Root->Right);
+	return FindMaxOfTwoNums(LeftTreeHeight, RightTreeHeight) + 1;
+}
+
+void LevelOrderTraversal(BstNode *&Root)
+{
+	Queue q1(15);
+
+	BstNode *TempNode = Root;
+
+	while (TempNode != nullptr) {
+		std::cout << TempNode->Data << " ";
+		q1.EnQueue(TempNode->Left);
+		q1.EnQueue(TempNode->Right);
+		TempNode = q1.DeQueue();
+	}
+	
+}
+
+Queue::Queue(int NewCapacity)
+{
+	Capacity = NewCapacity;
+
+	// Initialization of Array of Pointers to BstNode
+	Arr = new BstNode *[Capacity];
+	Size = 0;
+	Front = Rear = -1;
+}
+
+Queue::~Queue()
+{
+	delete []Arr;
+}
+
+bool Queue::IsEmpty()
+{
+	if (Size == 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+void Queue::EnQueue(BstNode *NewNode)
+{
+	if (!NewNode) { return; }
+
+	if (Size == 0) {
+		++Size;
+		++Front;
+		++Rear;
+		Arr[Rear] = NewNode;
+	} else {
+		++Size;
+		Rear = (Rear + 1) % Capacity;
+		Arr[Rear] = NewNode;
+	}
+}
+
+BstNode *Queue::DeQueue()
+{
+	if (IsEmpty()) { return nullptr; }
+
+	BstNode *Ret = Arr[Front];
+	Front = (Front + 1) % Capacity;
+	--Size;
+
+	return Ret;
+}
+
 int main() 
 {
 	BstNode *Root = nullptr;
@@ -48,8 +137,15 @@ int main()
 	Insert(Root, 10);
 	Insert(Root, 5);
 	Insert(Root, 17);
-
-	std::cout << IsSearchable(Root, 5) << IsSearchable(Root, 26) << std::endl;
+	/*
+	Root = SetNewNode(1);
+	Root->Left = SetNewNode(2);
+	Root->Right = SetNewNode(3);
+	Root->Left->Left = SetNewNode(4);
+	Root->Left->Right = SetNewNode(5);
+	*/
+	std::cout << IsSearchable(Root, 5) << IsSearchable(Root, 26) << FindTreeHeight(Root) << std::endl;
+	LevelOrderTraversal(Root);
 	return 0;
 }
 
